1b2606135c1a2123b723228a674b1b3235596607
sony93 <285955649@qq.com>
2018-02-07 16:07:57 +0800

    2-7

---
+++ src/LC/no278.java
@@ -0,0 +1,15 @@
+//package LC;
+//
+///**
+// * Created by sony on 18-2-7.
+// */
+//public class no278 {
+//    public int firstBadVersion(int n) {
+//        int left = 1;
+//        int right = n;
+//        int middle = (1 + n) / 2;
+//        while (left < right){
+//            if()
+//        }
+//    }
+//}
---
+++ src/LC/no292.java
@@ -0,0 +1,12 @@
+package LC;
+
+/**
+ * Created by sony on 18-2-7.
+ */
+public class no292 {
+    public boolean canWinNim(int n) {
+        if(n % 4 == 0)
+            return false;
+        return true;
+    }
+}
